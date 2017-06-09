7ae917426c7bee581ae840754edbc1adce22aabe
Michael Badaracca - Elastic Elephant <michael.badaracca@elasticElephant.com>
2017-06-08 23:03:15 -0600

    Created little string reversal page for a bud

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
-var mainAppVm = new Vue({
+
+
+var mainAppVm = new Vue( {
     el: '#app',
     data: {
-        test:"Web Page Rage!!!"
+        test: "Web Page Rage!!!",
+        //reversedString: "",
+        stringToReverse:"Is this what you meant bro?"
     },
 
     computed: {
+        reversedString: function ()
+        {
+            var reversed = "";
+            for ( var i = this.stringToReverse.length - 1; i >= 0 ; i-- )
+            {
+                reversed += this.stringToReverse[i];
+            }
+
+
+
+
+            //var test = "bah" +  this.stringToReverse;
+
+            return reversed;
+        }
     },
     methods: {
     }
