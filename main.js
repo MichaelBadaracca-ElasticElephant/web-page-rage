77473a8d9b374f08d8f5778785b7c96213f60d5d
Michael Badaracca - Elastic Elephant <michael.badaracca@elasticElephant.com>
2017-06-12 17:13:33 -0600

    Tried applying styles to iFrame/embeded content. Ran into cross-domain issues. Tried getting external page content on server side then sending it to the client to make it seem like it is from the same domain. Still TBD to see if this approach is viable.

--- main.js
+++ main.js
@@ -5,16 +5,15 @@ var mainAppVm = new Vue( {
     data: {
         test: "Web Page Rage!!!",
         pageToDisplay: "http://eloquentjavascript.net/",
+        pageHtml: "<div>Hello</div>",
         //reversedString: "",
-        stringToReverse:"Is this what you meant bro?"
+        stringToReverse: "Is this what you meant bro?"
     },
 
     computed: {
-        reversedString: function ()
-        {
+        reversedString: function () {
             var reversed = "";
-            for ( var i = this.stringToReverse.length - 1; i >= 0 ; i-- )
-            {
+            for ( var i = this.stringToReverse.length - 1; i >= 0; i-- ) {
                 reversed += this.stringToReverse[i];
             }
 
@@ -27,5 +26,11 @@ var mainAppVm = new Vue( {
         }
     },
     methods: {
+        getUrl: function () {
+            $.post( `/externalPage/`, { url: this.pageToDisplay }, function ( data ) {
+                this.pageHtml = data;
+                console.log( this.pageHtml );
+            })
+        }
     }
 })
--- server.js
+++ server.js
@@ -1,6 +1,36 @@
-var express = require('express')
-var app = express()
+var express = require( 'express' );
+var app = express();
+var bodyParser = require( 'body-parser' )
+var http = require( 'http' );
+var request = require( 'request' )
 
-app.use(express.static('./'))
 
-app.listen(80)
+
+
+app.use( express.static( './' ) );
+
+//TODO: use a different solution than body parser as it is depricated and presents a security risk according to this resource: http://andrewkelley.me/post/do-not-use-bodyparser-with-express-js.html
+app.use( bodyParser.urlencoded( { extended: false }) )
+
+
+//Not sure if this is RESTFul
+app.post( "/externalPage/", function ( req, res ) {
+    console.log( "Got to server", req.body )
+    request.get( 'https://google.com', function ( err, res2, body ) {
+        if ( err ) {
+            console.log( err );
+        } else {
+            console.log( body );
+            //res.status( 200 );
+            res.send( body );
+
+        }
+
+        //TODO Do something with response
+    });
+
+
+})
+
+
+app.listen( 8080 );
